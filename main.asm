2ddbdc85152fd8fcdaa003ddc87f0dd83cd40a8c
sandbox <ghostnether28@gmail.com>
2026-02-12 22:08:02 -0300

    aumento de arquivo

--- main.asm
+++ main.asm
@@ -2,15 +2,15 @@
 ; Headers.inc
 ; ==================================
 
-%include 
-%include
-%include
-%include
-%include
-%include
-%include
+%include sys_read           0 
+%include sys_write          1
+%include sys_open           2
+%include sys_close          3
+%include sys_getdents64     217
+%include sys_exit           60
+%include sys_kill           62
 
-%include
-%include
+%include stdout             1
+%include o_rdonly           0   
 
-%include
+%include buffer_size        4096 
