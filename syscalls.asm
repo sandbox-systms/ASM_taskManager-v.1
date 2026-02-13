c7bf044047353e6cd60b9ca854d9eed9fbbac527
sandbox <ghostnether28@gmail.com>
2026-02-12 21:59:25 -0300

    chamadas de syscalls

---
+++ syscalls.asm
@@ -0,0 +1,29 @@
+; ==================================
+; Syscalls.inc
+; ==================================
+
+global sys_write
+global sys_open
+global sys_getdents64
+global sys_exit
+
+section .text
+
+sys_write:
+    mov rax, sys_write
+    syscall 
+    ret
+
+sys_open:
+    mov rax, sys_open
+    syscall 
+    ret
+
+sys_getdents64:
+    mov rax, sys_getdents64
+    syscall 
+    ret
+
+sys_exit:
+    mov rax, sys_exit
+    syscall 
